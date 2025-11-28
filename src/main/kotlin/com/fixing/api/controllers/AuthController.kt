dbc225f59240aa23fe36b4c2f730f6fc0c0aaa79
GBLins14 <gabrielglins18@gmail.com>
2025-11-28 01:34:26 -0300

    Moving validators

--- src/main/kotlin/com/fixing/api/controllers/AuthController.kt
+++ src/main/kotlin/com/fixing/api/controllers/AuthController.kt
@@ -2,50 +2,22 @@ package com.fixing.api.controllers
 
 import com.fixing.api.configs.AuthConfig
 import com.fixing.api.models.CustomUserDetails
-import com.fixing.api.models.Role
 import com.fixing.api.models.User
 import com.fixing.api.repositories.UserRepository
 import com.fixing.api.schemas.UserSignInSchema
 import com.fixing.api.schemas.UserSignUpSchema
 import com.fixing.api.security.Hash
 import com.fixing.api.security.JwtUtil
+import com.fixing.api.utils.ValidatorUtil
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
-import kotlin.Long
-import kotlin.String
-
-fun String.isValidCpf(): Boolean {
-    val cleanedCpf = this.filter { it.isDigit() }
-    if (cleanedCpf.length != 11) return false
-    if (cleanedCpf.all { it == cleanedCpf[0] }) return false
-    val numbers = cleanedCpf.map { it.toString().toInt() }
-    val firstSum = (0 until 9).sumOf { (10 - it) * numbers[it] }
-    val firstDigit = (firstSum * 10 % 11).let { if (it == 10) 0 else it }
-    val secondSum = (0 until 10).sumOf { (11 - it) * numbers[it] }
-    val secondDigit = (secondSum * 10 % 11).let { if (it == 10) 0 else it }
-    return numbers[9] == firstDigit && numbers[10] == secondDigit
-}
-
-fun String.cleanCpf(): String = this.filter { it.isDigit() }
-
-fun String.isValidEmail(): Boolean {
-    val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$".toRegex()
-    return this.matches(emailRegex)
-}
-
-
-fun String.isValidPhone(): Boolean {
-    val phoneRegex = "^[0-9]{10,11}$".toRegex()
-    return this.matches(phoneRegex)
-}
 
 fun checkDuplicate(value: Any?, message: String): ResponseEntity<Any>? {
     return if (value != null) ResponseEntity.status(HttpStatus.CONFLICT).body(mapOf("success" to false, "message" to message)) else null
@@ -53,7 +25,7 @@ fun checkDuplicate(value: Any?, message: String): ResponseEntity<Any>? {
 
 @RestController
 @RequestMapping("/auth")
-class AccountController(private val authConfig: AuthConfig, private val userRepository: UserRepository, private val jwtUtil: JwtUtil, private val bcrypt: Hash) {
+class AuthController(private val authConfig: AuthConfig, private val userRepository: UserRepository, private val jwtUtil: JwtUtil, private val bcrypt: Hash, private val validatorUtil: ValidatorUtil) {
 
     @GetMapping("/me")
     fun me(): ResponseEntity<Any> {
@@ -85,55 +57,53 @@ class AccountController(private val authConfig: AuthConfig, private val userRepo
 
     @PostMapping("/sign-up")
     fun signUp(@RequestBody request: UserSignUpSchema): ResponseEntity<Any> {
-        /*
-                val cleanedCpf = request.cpf.cleanCpf()
+/*        val cleanedCpf = validatorUtil.cleanCpf(request.cpf)
 
-                if (!cleanedCpf.isValidCpf()) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(mapOf("success" to false, "message" to "É necessário inserir um número de CPF que seja válido."))
-                }
-        */
+        if (!validatorUtil.isValidCpf(cleanedCpf)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(mapOf("success" to false, "message" to "É necessário inserir um número de CPF que seja válido."))
+        }*/
 
         if (request.username.length < authConfig.minUsernameLength || request.username.length > authConfig.maxUsernameLength) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(mapOf("success" to false, "message" to "O nome de usuário deve conter no mínimo ${authConfig.minUsernameLength} caracteres, e no máximo ${authConfig.maxUsernameLength} caracteres."))
         }
 
-        if (!request.email.isValidEmail()) {
+        if (!validatorUtil.isValidEmail(request.email)) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(mapOf("success" to false, "message" to "É necessário inserir um endereço de email que seja válido."))
         }
 
-        /*        if (!request.phone.isValidPhone()) {
-                    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                        .body(mapOf("success" to false, "message" to "É necessário inserir um número de telefone que seja válido."))
-                }*/
+/*        if (!validatorUtil.isValidPhone(request.phone)) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(mapOf("success" to false, "message" to "É necessário inserir um número de telefone que seja válido."))
+        }*/
 
         if (request.password.length < authConfig.minPasswordLength || request.password.length > authConfig.maxPasswordLength) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(mapOf("success" to false, "message" to "A senha deve conter no mínimo ${authConfig.minPasswordLength} caracteres, e no máximo ${authConfig.maxPasswordLength} caracteres."))
         }
 
-        /*        val existingCpf = userRepository.findByCpf(cleanedCpf)*/
+//        val existingCpf = userRepository.findByCpf(cleanedCpf)
         val existingUsername = userRepository.findByUsername(request.username)
         val existingEmail = userRepository.findByEmail(request.email)
-        /*        val existingPhone = userRepository.findByPhone(request.phone)*/
+//        val existingPhone = userRepository.findByPhone(request.phone)
 
-        /*        checkDuplicate(existingCpf, "Já existe uma conta registrada com este número de CPF.")?.let { return it }*/
+//        checkDuplicate(existingCpf, "Já existe uma conta registrada com este número de CPF.")?.let { return it }
         checkDuplicate(existingUsername, "Já existe uma conta registrada com este nome de usuário.")?.let { return it }
         checkDuplicate(existingEmail, "Já existe uma conta registrada com este endereço de email.")?.let { return it }
-        /*        checkDuplicate(existingPhone, "Já existe uma conta registrada com este número de telefone.")?.let { return it }*/
+//        checkDuplicate(existingPhone, "Já existe uma conta registrada com este número de telefone.")?.let { return it }
 
         val user = User(
-            /*            cpf = cleanedCpf,*/
-            /*            name = request.name,*/
+//            cpf = cleanedCpf,
+//            name = request.name,
             username = request.username,
             email = request.email,
-            /*            phone = request.phone,*/
+//            phone = request.phone,
             hashedPassword = bcrypt.encodePassword(request.password)
         )
 
-        val account = userRepository.save(user)
+        userRepository.save(user)
         return ResponseEntity.status(HttpStatus.CREATED).body(mapOf("success" to true, "message" to "Conta registrada com sucesso."))
     }
 
